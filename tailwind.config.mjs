04f58bf2c863e8a9073194051902788bb5d4e732
Sebastian Molina <smolinadunal.edu.co>
2024-05-23 17:28:17 -0500

    New landing with Astro

---
+++ tailwind.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			'animation': {
+				'text':'text 5s ease infinite',
+			},
+			'keyframes': {
+				'text': {
+					'0%, 100%': {
+					   'background-size':'100% 100%',
+						'background-position': 'left center'
+					},
+					'50%': {
+					   'background-size':'100% 100%',
+						'background-position': 'right center'
+					}
+				},
+			}
+		},
+	},
+	plugins: [],
+}
